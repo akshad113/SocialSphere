5910ff496e8b9f37cf771c460fd7507f83de2581
Akshad <Akshad113@gmail.com>
2023-08-25 20:34:40 +0530

    sign up page code

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user')
 module.exports.profile = function(req,res){
     res.render('users_profile',{title:'users profile'});
 }
@@ -9,3 +10,22 @@ module.exports.signIn = function(req,res){
 module.exports.signUp = function(req,res){
     res.render('users_sign_up',{title:'create account'});
 }
+module.exports.create = async function(req, res) {
+    try {
+        if (req.body.password !== req.body.confirm_password) {
+            return res.redirect('/users/sign-up');
+        }
+
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            await User.create(req.body);
+            return res.redirect('/');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (error) {
+        console.error("Error while creating user:", error);
+        return res.redirect('back');
+    }
+};
